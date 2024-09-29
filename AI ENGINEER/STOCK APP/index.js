182c6fc7f0b10a9e3c6e8d562bbe3afbfb6339bd
Sourabh <sourabhbeniwal@outlook.com>
2024-09-29 15:27:27 +1000

    polygon api and vite config setup

--- AI ENGINEER/STOCK APP/index.js
+++ AI ENGINEER/STOCK APP/index.js
@@ -1,32 +1,107 @@
-import "dotenv/config";
+import { dates } from "/utils/dates";
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+// const openai = new OpenAI({
+//   apiKey: process.env.VITE_OPENAI_API_KEY,
+// });
+
+// const messages = [
+//   {
+//     role: "system",
+//     content: "You are a knowledge assistant with expert advice on subject matter",
+//   },
+//   {
+//     role: "user",
+//     content: "Who invented the first TV",
+//   },
+// ];
+
+// const response = await openai.chat.completions.create({
+//   model: "gpt-3.5-turbo",
+//   messages: messages,
+//   temperature: 1,
+//   max_tokens: 500,
+//   top_p: 1,
+//   frequency_penalty: 0,
+//   presence_penalty: 0,
+//   response_format: {
+//     type: "text",
+//   },
+// });
+
+const tickersArr = [];
+
+const generateReportBtn = document.querySelector(".generate-report-btn");
+
+generateReportBtn.addEventListener("click", fetchStockData);
+
+document.getElementById("ticker-input-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const tickerInput = document.getElementById("ticker-input");
+  if (tickerInput.value.length > 2) {
+    generateReportBtn.disabled = false;
+    const newTickerStr = tickerInput.value;
+    tickersArr.push(newTickerStr.toUpperCase());
+    tickerInput.value = "";
+    renderTickers();
+  } else {
+    const label = document.getElementsByTagName("label")[0];
+    label.style.color = "red";
+    label.textContent =
+      "You must add at least one ticker. A ticker is a 3 letter or more code for a stock. E.g TSLA for Tesla.";
+  }
 });
 
-const messages = [
-  {
-    role: "system",
-    content: "",
-  },
-  {
-    role: "user",
-    content: "Who invented the first TV",
-  },
-];
+function renderTickers() {
+  const tickersDiv = document.querySelector(".ticker-choice-display");
+  tickersDiv.innerHTML = "";
+  tickersArr.forEach((ticker) => {
+    const newTickerSpan = document.createElement("span");
+    newTickerSpan.textContent = ticker;
+    newTickerSpan.classList.add("ticker");
+    tickersDiv.appendChild(newTickerSpan);
+  });
+}
 
-const response = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo",
-  messages: messages,
-  temperature: 1,
-  max_tokens: 500,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-  response_format: {
-    type: "text",
-  },
-});
+const loadingArea = document.querySelector(".loading-panel");
+const apiMessage = document.getElementById("api-message");
 
-console.log(response.choices[0].message.content);
+async function fetchStockData() {
+  document.querySelector(".action-panel").style.display = "none";
+  loadingArea.style.display = "flex";
+  try {
+    const stockData = await Promise.all(
+      tickersArr.map(async (ticker) => {
+        const url = `https://api.polygon.io/v2/aggs/ticker/${ticker}/range/1/day/${
+          dates.startDate
+        }/${dates.endDate}?apiKey=${import.meta.env.VITE_POLYGON_API_KEY}`;
+        const response = await fetch(url);
+        const data = await response.text();
+        const status = await response.status;
+        if (status === 200) {
+          apiMessage.innerText = "Creating report...";
+          return data;
+        } else {
+          loadingArea.innerText = "There was an error fetching stock data.";
+        }
+      })
+    );
+    fetchReport(stockData.join(""));
+  } catch (err) {
+    loadingArea.innerText = "There was an error fetching stock data.";
+    console.error("error: ", err);
+  }
+}
+
+async function fetchReport(data) {
+  /** AI goes here **/
+}
+
+function renderReport(output) {
+  loadingArea.style.display = "none";
+  const outputArea = document.querySelector(".output-panel");
+  const report = document.createElement("p");
+  outputArea.appendChild(report);
+  report.textContent = output;
+  outputArea.style.display = "flex";
+}
