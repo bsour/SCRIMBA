2ffa7ea093ffbf5bbd6de9e5668ee90b8bd73452
Sourabh <sourabhbeniwal@outlook.com>
2024-10-10 09:50:35 +1100

    semantic search using vector embeddings

--- AI ENGINEER/Semantic-search-using-Embedding/content.js
+++ AI ENGINEER/Semantic-search-using-Embedding/content.js
@@ -1,5 +1,5 @@
 // Fake podcast titles and descriptions
-export default [
+const podcasts = [
   "Beyond Mars (1 hr 15 min): Join space enthusiasts as they speculate about extraterrestrial life and the mysteries of distant planets.",
   "Jazz under stars (55 min): Experience a captivating night in New Orleans, where jazz melodies echo under the moonlit sky.",
   "Mysteries of the deep (1 hr 30 min): Dive with marine explorers into the uncharted caves of our oceans and uncover their hidden wonders.",
@@ -9,5 +9,5 @@ export default [
   "Decoding dreams (1 hr 22 min): Step into the realm of the subconscious, deciphering the intricate narratives woven by our dreams.",
   "Time capsules (50 min): Revel in the bizarre, endearing, and profound discoveries that unveil the quirks of a century past.",
   "Frozen in time (1 hr 40 min): Embark on an icy expedition, unearthing secrets hidden within the majestic ancient glaciers.",
-  "Songs of the Sea (1 hr): Dive deep with marine biologists to understand the intricate whale songs echoing in our vast oceans.",
+  "Songs of the Sea (1 hr): Dive deep with marine biologists to understand the intricate whale songs echoing in our vast oceans."
 ];
